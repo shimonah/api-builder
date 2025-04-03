41a4fb1226550fbb0954841d226ce8b705811cd6
shimonah <=>
2025-04-03 23:53:13 +0100

    fixed inputs endpoints

--- src/inputs/inputs.service.ts
+++ src/inputs/inputs.service.ts
@@ -15,36 +15,23 @@ export class InputsService {
   ) {}
 
   async create(createInputDto: CreateInputDto): Promise<Input> {
-    // Create the input without the endpoint_id
-    const { endpoint_id, ...inputData } = createInputDto;
+    const { ...inputData } = createInputDto;
     const input = this.inputsRepository.create(inputData);
     
-    // Save the input first
-    const savedInput = await this.inputsRepository.save(input);
-    
-    // If endpoint_id is provided, associate the input with the endpoint
-    if (endpoint_id !== undefined) {
-      const endpoint = await this.endpointsService.findById(endpoint_id);
-      
-      // Add the input to the endpoint's inputs
-      if (!endpoint.inputs) {
-        endpoint.inputs = [];
-      }
-      endpoint.inputs.push(savedInput);
-      await this.endpointsService.saveEndpoint(endpoint);
-    }
-    
-    return savedInput;
+    return await this.inputsRepository.save(input);
   }
 
+  // TODO: refine and check if relation is needed, same for findOne, findByCode
   async findAll(): Promise<Input[]> {
-    return this.inputsRepository.find({ relations: ['endpoints'] });
+    return this.inputsRepository.find({
+      relations: []
+    });
   }
 
   async findOne(id: number): Promise<Input> {
     const input = await this.inputsRepository.findOne({
       where: { id },
-      relations: ['endpoints'],
+      relations: [],
     });
 
     if (!input) {
@@ -57,7 +44,6 @@ export class InputsService {
   async findByEndpointId(endpoint_id: number): Promise<Input[]> {
     const endpoint = await this.endpointsService.findById(endpoint_id);
     
-    // Load the inputs relation
     await this.endpointsService.loadEndpointRelations(endpoint, ['inputs']);
     
     return endpoint.inputs || [];
@@ -66,30 +52,10 @@ export class InputsService {
   async update(id: number, updateInputDto: UpdateInputDto): Promise<Input> {
     const input = await this.findOne(id);
     
-    // If endpoint_id is being updated, handle the relationship change
-    if (updateInputDto.endpoint_id) {
-      // This is now handled differently with many-to-many
-      // We'll need to update the endpoint's inputs collection
-      const endpoint = await this.endpointsService.findById(updateInputDto.endpoint_id);
-      
-      // Remove endpoint_id from the DTO as it's not a field in the entity anymore
-      const { endpoint_id, ...inputData } = updateInputDto;
-      
-      // Update the input data
-      Object.assign(input, inputData);
-      
-      // Save the input
-      const savedInput = await this.inputsRepository.save(input);
-      
-      // Update the endpoint's inputs if needed
-      // This would require additional logic to add/remove from the relationship
-      
-      return savedInput;
-    } else {
-      // Just update the input data
-      Object.assign(input, updateInputDto);
-      return this.inputsRepository.save(input);
-    }
+    Object.assign(input, updateInputDto);
+    
+    return this.inputsRepository.save(input);
+    
   }
 
   async remove(id: number): Promise<void> {
@@ -100,7 +66,7 @@ export class InputsService {
   async findByCode(code: string): Promise<Input> {
     const input = await this.inputsRepository.findOne({
       where: { code },
-      relations: ['endpoints'],
+      relations: [],
     });
 
     if (!input) {
